b1fa5a871d5de9714b0c0315167f2036bdcc4800
alfredo.holz.junior <alfredojrgasper@gmail.com>
2023-03-07 23:18:17 -0300

    major : update django

--- djangotheforcebe/urls.py
+++ djangotheforcebe/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
 from django.contrib import admin
 from django.urls import include, path
 
@@ -22,7 +22,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from shop import urls as urls_shops
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('shop/', include(urls_shops),name='shop'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
